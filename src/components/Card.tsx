5a799b29def95f4956649e7af0054b7925b92dc9
Lucio Gastellu Arrieta <104010048+lucio1907@users.noreply.github.com>
2022-09-28 00:48:22 -0300

    form created

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import Form from "./Form"
 const Card = () => {
     return (
         <div className='flex justify-center items-center h-screen'>
-            <div className='flex flex-col items-center justify-center gap-12 h-96 bg-slate-100 p-10'>
+            <div className='flex flex-col items-center justify-center gap-12 h-96 bg-slate-100 p-10 rounded-md'>
                 <h1 className="font-bold uppercase">Save your Task!</h1>
                 <Form />
             </div>
