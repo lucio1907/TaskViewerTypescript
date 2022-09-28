c09460d84706339298b7514968e824214e551063
Lucio Gastellu Arrieta <104010048+lucio1907@users.noreply.github.com>
2022-09-28 12:27:12 -0300

    Fixes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const Card = ({ setTasks, tasks }: Props) => {
     return (
-        <div className='flex justify-center mt-10'>
+        <div className='flex justify-center mt-20'>
             <div className='flex flex-col items-center justify-center gap-12 h-96 bg-slate-100 p-10 rounded-md md:w-2/4'>
                 <h1 className="font-bold uppercase">Save your Task!</h1>
                 <Form setTasks={setTasks} tasks={tasks} />
